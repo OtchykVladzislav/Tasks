de5fd4c4629e680682b2e9f428f2b848434f2164
Отчик Влад <79385087+OtchykVladzislav@users.noreply.github.com>
2022-01-01 22:22:30 +0300

    Task 7 part 2
    Все работает и комментарии написаны

--- task7/js/Game.js
+++ task7/js/Game.js
@@ -1,47 +1,43 @@
 const open = document.getElementById("trashOp"); 
-const close = document.getElementById("trashCl")
-
+const close = document.getElementById("trashCl");
+const div = document.getElementById("trash");
+let element;
+//garbage is located randomly(4 elements)
 function trash() {
     for(let i=0; i < 4; i++){
-        let div = document.getElementById("trash");
         let img = document.createElement("img");
-        img.id = "trash-"+(i+1);
+        div.appendChild(img);
         img.src = "images/trash.jpg";
-        img.draggable = "true";
-        img.ondrag="onDragStart(event);"
+        img.id = "trash-"+(i+1);
+        //Indicates whether the element can be dragged
+        img.setAttribute('draggable','true');
+        //the user started dragging the element
+        img.addEventListener('dragstart',function(event){element = event.target.id;})
         let left = Math.floor(Math.random()*1200);
         let top = Math.floor(Math.random()*800);
-        img.style.position = "fixed";
+        img.style.position = "absolute";
         img.style.width = "150px";
         img.style.height = "150px";
         img.style.left = left + "px";
         img.style.top = top + "px";
         img.style.cursor = "grab";
-        div.appendChild(img);
     }
 }
 
-function onDragStart(event) {
-    event.dataTransfer.setData("text/plain", event.target.id);
-}
-
+//the dragged item leaves a valid drop target.
 function onDragLeave(event){
     open.style.display = "none";
     close.style.display = "block";
 }
-
+//the item is dragged over a valid target
 function onDragOver(event) {
     event.preventDefault();
     open.style.display = "block";
     close.style.display = "none";
 }
-
+//the element has been dropped into the allowable drop zone
 function onDrop(event) {
-    const data = event.dataTransfer.getData("text");
-    console.log(data);
-    const draggableElemnt = document.getElementById(data);
-    draggableElemnt.style.display = "none";
+    document.getElementById(element).style.display = "none";
     open.style.display = "none";
     close.style.display = "block";
-    event.dataTransfer.clearData();
 }
